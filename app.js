124abf44fbfd8cbdcd56d762b4d301ab8081fb0e
chris <chrszhu@yahoo.com>
2018-10-03 16:00:00 -0700

    added error handling

--- app.js
+++ app.js
@@ -23,8 +23,14 @@ request({
   url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${config.key}`,
   json: true
 }, (error, response, body) => {
-  console.log(`Address: ${body.results[0].formatted_address}`)
-  console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
-  console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
+  if (error) {
+    console.log('Unable to connect to Google servers.');
+  } else if (body.status === 'ZERO_RESULTS') {
+    console.log('Unable to find that address.')
+  } else if (body.status === 'OK') {
+     console.log(`Address: ${body.results[0].formatted_address}`)
+     console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
+     console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
+  }
 
 });
